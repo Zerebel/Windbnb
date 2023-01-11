78e921601f4a9cfda78912b6c2f26df3d31d7046
Zerebel <86872654+Zerebel@users.noreply.github.com>
2023-01-11 10:33:59 +0000

    header complete

--- windbnb/src/App.jsx
+++ windbnb/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import data from "./data/stays.json";
+import windbnb from "./assets/logo.svg";
+const Image = ({ image, alt, className }) => (
+  <img src={image} alt={alt} className={`w-full ${className}`} />
+);
+
+const Anchor = ({ link, text, target }) => (
+  <a href={link} target={target}>
+    {text}
+  </a>
+);
+
+const Button = ({ text, className, onClick }) => (
+  <button onClick={() => onClick} className={className}>
+    {text}
+  </button>
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="mx-4 md:mx-8 lg:mx-12 my-4 md:my-8">
+      <header className="flex justify-between flex-col md:flex-row gap-8 md:gap-0">
+        <div className=" self-start md:self-center">
+          <Image image={windbnb} alt="windbnb" />
+        </div>
+        <div className="self-center md:self-auto font-mulish">
+          <div className="shadow-box rounded-2xl flex px-3 gap-2 justify-center text-sm leading-4">
+            <Button
+              text={"Helsinki, Finland"}
+              className="py-4 text-[#333333]"
+            />
+            <div className=" border-l-[0.0625rem] border-l-[#f2f2f2]"></div>
+            <Button text={"Add guests"} className="text-[#BDBDBD]" />
+            <div className=" border-l-[0.0625rem] border-l-[#f2f2f2]"></div>
+            <span className="material-icons self-center text-[#EB5757]">
+              search
+            </span>
+          </div>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- windbnb/tailwind.config.cjs
+++ windbnb/tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
+        mulish: ["Mulish", ...defaultTheme.fontFamily.sans],
+        montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        xxs: "",
+      },
+      boxShadow: {
+        box: "0px 1px 6px rgba(0, 0, 0, 0.1)",
+      },
+    },
   },
   plugins: [],
-}
+};
