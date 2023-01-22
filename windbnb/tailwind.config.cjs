eabe9700057058fd3446cb0ec35029e59b87c55c
Zerebel <86872654+Zerebel@users.noreply.github.com>
2023-01-22 12:27:08 +0000

    completed

--- windbnb/tailwind.config.cjs
+++ windbnb/tailwind.config.cjs
@@ -3,6 +3,16 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./index.html", "./src/**/*.{jsx,js}"],
   theme: {
+    screens: {
+      md: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       fontFamily: {
         poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
@@ -10,7 +20,7 @@ module.exports = {
         montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
       },
       fontSize: {
-        xxs: "",
+        xxs: "0.625rem",
       },
       boxShadow: {
         box: "0px 1px 6px rgba(0, 0, 0, 0.1)",
